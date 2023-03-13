8c9b2b4b2c0261bcd7e90e65ff1145dd6c2e5fbc
Flavio <alvaradotuctod@gmail.com>
2023-03-13 16:19:46 -0500

    refactor

---
+++ src/pages/api/users.ts
@@ -0,0 +1,22 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export const config = {
+  runtime: 'edge'
+};
+
+
+export default (req: NextRequest) => {
+  return NextResponse.json({
+    ok: true,
+    data: [
+      {
+        id: 1,
+        name: 'Linder'
+      },
+      {
+        id: 2,
+        name: 'Lucas'
+      }
+    ]
+  })
+}
